89f4e7970217312766f3bf24a9757f0215d49d0a
Alexis Gutiérrez <agutierrez@tecnoaccion.corp>
2023-03-13 14:27:29 -0300

    a

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import './App.css';
 
 function App() {
   const handleClick = () => {
-    let asd = 'excel.xls'
+    let clientes = ["ALT", "LP", "LS"]
+    let str = ""
+    let token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBZG1pbmlzdHJhZG9yMiIsImV4cCI6MTY3ODc0NjM3MiwiaWF0IjoxNjc4NzI4MzcyfQ.BFk6oWiLLchzNHXoxOzi-UUsgOLtIFPBU_1UxNPhhrzk1Luu8yvUShttScvKWEvPPteCjnvZ_8cXvI0Mz32ulg"
+    // let asd = 'excel.xls'
+    str += `comprobantes-comprobante-`
+    clientes.map((cliente, index) => {
+      str += `${cliente}${(index === clientes.length - 1) ? "" : "-"}`
+    })
+    console.log(str)
     saveAs(
       'http://desa-fabi.tecnoaccion.com.ar/api-user/reportes/getReportesComprobantesFile?anioId=2022&mesId=12&clienteId=ALT,LP',
-      asd,
-      { Accept: "application/octet-stream" }
+      `${str}.xls`,
+      {
+        Accept: "application/octet-stream",
+        Authorization: "Bearer " + token,
+      }
     )
   }
   return (
