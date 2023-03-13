007e2e9a6ce6a6424836761916d135186ca76ddc
Alexis Gutiérrez <agutierrez@tecnoaccion.corp>
2023-03-13 14:02:05 -0300

    change filename

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 
 function App() {
   const handleClick = () => {
-    let asd = 'imagen'
+    let asd = 'imagen.png'
     saveAs(
       'https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2021/09/WhatsApp-logo-12.png?fit=1200%2C771&quality=60&strip=all&ssl=1',
-      `${asd}.png`,
+      asd,
       { Accept: "image/png" }
     )
   }
