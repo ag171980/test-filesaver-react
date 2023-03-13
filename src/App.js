3c31a3a116459be655f39af707f614c1a294bb25
Alexis Gutiérrez <agutierrez@tecnoaccion.corp>
2023-03-13 14:48:26 -0300

    .

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     console.log(str)
     saveAs(
       'https://d500.epimg.net/cincodias/imagenes/2018/11/13/lifestyle/1542113135_776401_1542116070_noticia_normal.jpg',
-      `${str}`,
+      str,
       {
         Accept: `application/${formato === "Excel" ? 'octet-stream' : 'x-pdf'}`,
         Authorization: "Bearer " + token,
